29b1fd5d518c1fe83b97a9e9a1b6ddda5493c685
silviofranco <silvio.franco@sicoob.com.br>
2024-07-04 08:51:01 -0400

    Iniciando projeto

---
+++ src/test/kotlin/br/com/silviofranco/RestWithSpringBootAndKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.silviofranco
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestWithSpringBootAndKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
