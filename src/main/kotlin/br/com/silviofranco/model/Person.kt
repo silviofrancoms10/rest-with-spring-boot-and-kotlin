f7ae5f94754acd9b259228ec98db56f1d865586f
silviofranco <silviofrancoms@hotmail.com>
2024-07-17 12:09:07 -0400

    Entidade Person e Repository

--- src/main/kotlin/br/com/silviofranco/model/Person.kt
+++ src/main/kotlin/br/com/silviofranco/model/Person.kt
@@ -1,9 +1,25 @@
 package br.com.silviofranco.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "person")
 data class Person (
-    var id: Long = 0,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long =0,
+    @Column(name = "first_name", nullable = false, length = 80)
     var firstName: String = "",
+    @Column(name = "last_name", nullable = false, length = 80)
     var lastName: String = "",
+    @Column(nullable = false, length = 100)
     var address: String = "",
+    @Column(nullable = false, length = 6)
     var gender: String = ""
 )
