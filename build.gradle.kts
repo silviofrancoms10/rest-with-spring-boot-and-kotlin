48cff6b86040f34f81d355ab4d60287813341e88
silviofranco <silviofrancoms@hotmail.com>
2024-07-17 11:53:32 -0400

    Apontamento para o banco de dados

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.33")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
