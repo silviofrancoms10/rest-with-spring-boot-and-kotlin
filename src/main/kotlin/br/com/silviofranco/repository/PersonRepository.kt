649a266c1c42f47b2f01576fb61c2158222174c1
silviofrancoms10 <silviofrancoms@gmail.com>
2024-08-22 23:44:56 -0400

    Não é mais necessário anotar a interface Repository com @Repository, ao herdar JpaRepository o spring entende

--- src/main/kotlin/br/com/silviofranco/repository/PersonRepository.kt
+++ src/main/kotlin/br/com/silviofranco/repository/PersonRepository.kt
@@ -2,8 +2,6 @@ package br.com.silviofranco.repository
 
 import br.com.silviofranco.model.Person
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface PersonRepository: JpaRepository<Person, Long?>{
 }
