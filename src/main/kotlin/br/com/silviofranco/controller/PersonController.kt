ca0748d65783ccab3f34c1e072f94dfe94159882
silviofrancoms10 <silviofrancoms@gmail.com>
2024-08-22 23:17:31 -0400

    Refatorando anotacoes para ficar mais visivel e intuitivo, retornando noContent no Delete

--- src/main/kotlin/br/com/silviofranco/controller/PersonController.kt
+++ src/main/kotlin/br/com/silviofranco/controller/PersonController.kt
@@ -1,16 +1,11 @@
 package br.com.silviofranco.controller
 
-import br.com.silviofranco.converters.NumberConverter
 import br.com.silviofranco.model.Person
 import br.com.silviofranco.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
-import java.lang.UnsupportedOperationException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/person")
@@ -19,36 +14,34 @@ class PersonController {
     @Autowired
     private lateinit var service: PersonService
 
-    @RequestMapping(method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findByAll(): List<Person> {
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun findAll(): List<Person> {
         return service.findAll()
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET],
+    @GetMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findById(@PathVariable(value = "id") id: Long): Person {
         return service.findById(id)
     }
 
-    @RequestMapping(method = [RequestMethod.POST],
-        consumes = [MediaType.APPLICATION_JSON_VALUE] ,
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody person: Person): Person {
         return service.create(person)
     }
 
-    @RequestMapping(method = [RequestMethod.PUT],
-        consumes = [MediaType.APPLICATION_JSON_VALUE] ,
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE])
     fun update(@RequestBody person: Person): Person {
         return service.update(person)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE],
+    @DeleteMapping(value = ["/{id}"],
         produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun delete(@PathVariable(value = "id") id: Long) {
-        return service.delete(id)
+    fun delete(@PathVariable(value = "id") id: Long) : ResponseEntity<*> {
+        service.delete(id)
+        return ResponseEntity.noContent().build<Any>()
     }
 
 }
