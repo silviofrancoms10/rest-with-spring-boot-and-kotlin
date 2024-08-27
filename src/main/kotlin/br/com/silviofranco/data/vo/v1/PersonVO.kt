022625c6a9eeec8ec193b4e8c5c70fe3f31464c8
silviofrancoms10 <silviofrancoms@gmail.com>
2024-08-26 23:44:10 -0400

    Customização da Serialização do JSON

--- src/main/kotlin/br/com/silviofranco/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/silviofranco/data/vo/v1/PersonVO.kt
@@ -1,10 +1,18 @@
 package br.com.silviofranco.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder("id", "address", "firstName", "lastName","gender") // serve para retornar o objeto nessa sequencia na requisição
 data class PersonVO (
 
     var id: Long =0,
+    @field:JsonProperty("first_name")  // serve para retornar o objeto com esse nome na requisição Postman
     var firstName: String = "",
+    @field:JsonProperty("last_name")    // serve para retornar o objeto com esse nome na requisição Postman
     var lastName: String = "",
     var address: String = "",
+    @field:JsonIgnore   // omitir no retorno
     var gender: String = ""
 )
