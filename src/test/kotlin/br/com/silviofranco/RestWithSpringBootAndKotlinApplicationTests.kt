562afa4893afc87b4516cf7e8d7f5f176793b00b
silviofrancoms10 <silviofrancoms@gmail.com>
2024-09-23 17:28:52 -0400

    removendo o teste vazio que estava causando problema no docker compose

--- src/test/kotlin/br/com/silviofranco/RestWithSpringBootAndKotlinApplicationTests.kt
+++ src/test/kotlin/br/com/silviofranco/RestWithSpringBootAndKotlinApplicationTests.kt
@@ -1,13 +1,12 @@
 package br.com.silviofranco
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class RestWithSpringBootAndKotlinApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+//    @Test
+//    fun contextLoads() {
+//    }
 
 }
